97f4d856d5ba57ef16bcb1a92b4c338d69c4fda8
psend <psenderski94@gmail.com>
2026-07-22 20:06:38 +0200

    test: add ZIP archive creation tests

---
+++ tests/test_archiver.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from zipfile import ZipFile
+
+from zip_archiver.archiver import ZipArchiver
+from zip_archiver.models import ArchiveEntry
+
+
+def test_create_archive(tmp_path: Path) -> None:
+    """Should create a ZIP archive."""
+
+    file = tmp_path / "document.txt"
+    file.write_text("Hello")
+
+    plan = ArchiveEntry(
+        year=2025,
+        archive_name="archive.zip",
+        files=[file],
+    )
+
+    archiver = ZipArchiver()
+
+    archive = archiver.create_archive(tmp_path, plan,)
+
+    assert archive.exists()
+
+    with ZipFile(archive) as zip_file:
+        assert "document.txt" in zip_file.namelist()
